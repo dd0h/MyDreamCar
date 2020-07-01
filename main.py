52332a5c864abe1445bc1bc99a1fb675fc6f4db4
dd0h <ddoh@o2.pl>
2020-07-01 12:14:41 +0200

    Final version release

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from Classes import Game
 
 def main():
     pygame.init()
-    pygame.display.set_caption("MyDreamCar v.0.97")
+    pygame.display.set_caption("MyDreamCar v.1.0")
 
     game = Game.Game()
     game.run()
